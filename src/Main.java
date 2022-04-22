14f3c993b539931349205221aa95e22e8c591d4b
zea321 <zea123@mail.ru>
2022-04-22 21:10:41 +0300

    Немного изменил подход к написанию кода, старался сделать его меньше.

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        char one = 49;
+System.out.println("Задание " + one);
+    byte myAge = 29;
+    short speedInformationBrainKmh = 432;
+    int speedLight = 299_792_458;
+    long myDick = 9999999999999999L;
+    float speedSoundKmh = 1.224f;
+    double speedLightningMs = 10000000.0;
+    boolean speedLightTrue = speedLight == 299_792_458;
+    System.out.println("Мне " + myAge + " и я люблю науку!"
+            + "И вот несколько интересных фактов из мира науки." + " Скорость сигнала в нашем мозге "
+            + speedInformationBrainKmh + " Км/ч!!" + " Скорость света " + 299_792_458 + " км/с. " + " Длин*** ЦЕНЗУРА!!" +
+            myDick + " Планковских длин)) " +  " Звук движется в воздухе температоруй 20 градусов, со скоростью " +
+            speedSoundKmh + " Км/ч" + " А скорость молнии " + speedLightningMs + " М/с." );
+    System.out.println("Is it true that the speed of light is equal to the speed of radio waves? " + speedLightTrue + "!");
+System.out.println("Теперь давайте сменим тему))" + " Задание 2");
+
+    float chan = 78.2f;
+    float ipMan = 82.7f;
+    System.out.println("Вес обоих бойцов = " + (chan + ipMan));
+    System.out.println("Задание 3");
+
+        byte banana = 5 * 8;
+        short milk = 200 / 100 * 105;
+        short iceCream = 100 * 2;
+        short eeg = 4 * 70;
+        int proteinSheyk = banana + milk + eeg + iceCream;
+        float proteinSheykKg = proteinSheyk / 1000f;
+        System.out.println("Вес коктеля  = " + proteinSheykKg + " кг!");
+        System.out.println("Задание 4");
+
+        short min = 7000 / 250;
+        short max = 7000 / 500;
+        int medium = (min + max) / 2;
+        System.out.println("Миниму понадобится " + min + " дней. " + " Максимум " + max + " дней. " + "Но в среднем " + medium + " день.");
+
+        System.out.println("Задание 5");
+        int mashaZp = 67_760;
+        int mashaPremiyZp = mashaZp / 10;
+        int denisZp = 83_690;
+        int denisPremyZp = denisZp / 10;
+        int kristinaZp = 76_230;
+        int kristinaPremyZp = kristinaZp / 10;
+        System.out.println(" ");
+        System.out.println("Теперь Маша получает " + (mashaZp + mashaPremiyZp) +  " рублей в месяц." + " Ее зп увеличилась на " + mashaPremiyZp + " Рублей.");
+        System.out.println("Денис получает " + (denisZp + denisPremyZp) + " рублей в месяц," + " это на " + denisPremyZp + " больше чем годом ранее.");
+        System.out.println("Кристина залила нам сервера, и поэтому не получит " + (kristinaZp + kristinaPremyZp) + " рублей за этот месяц," + " что на " + kristinaPremyZp + " Больше чем обычно.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
+
+
+
+
+
